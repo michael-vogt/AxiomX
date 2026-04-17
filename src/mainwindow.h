415b7335a571e89060241dd2f8a03cc69b50190c
Michael Vogt <m.vogt@lvm.de>
2026-04-17 13:26:19 +0200

    feature complete

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -48,6 +48,7 @@ public:
         LineTool* lineTool = new LineTool(controller, interaction, scene, commandManager);
         MoveTool* moveTool = new MoveTool(controller);
         SelectTool* selectTool = new SelectTool(controller, selection, scene);
+        CircleTool* circleTool = new CircleTool(controller, interaction, scene, commandManager);
 
         // Toolbar
         QToolBar* toolbar = addToolBar("Tools");
@@ -60,21 +61,25 @@ public:
         QAction* moveAction = toolbar->addAction("Move");
         QAction* pointAction = toolbar->addAction("Point");
         QAction* lineAction = toolbar->addAction(lineTypeToString(lineTool->lineType()));
+        QAction* circleAction = toolbar->addAction("Circle");
 
         selectAction->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_S));
         moveAction->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_M));
         pointAction->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_P));
         lineAction->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_L));
+        circleAction->setShortcut(QKeySequence(Qt::CTRL | Qt::Key_C));
 
         selectAction->setCheckable(true);
         moveAction->setCheckable(true);
         pointAction->setCheckable(true);
         lineAction->setCheckable(true);
+        circleAction->setCheckable(true);
 
         group->addAction(selectAction);
         group->addAction(moveAction);
         group->addAction(pointAction);
         group->addAction(lineAction);
+        group->addAction(circleAction);
 
         // Default Tool
         moveAction->setChecked(true);
@@ -100,6 +105,10 @@ public:
             } else {
                 m_view->setTool(lineTool);
             }
+        });
+
+        connect(circleAction, &QAction::triggered, [this, circleTool]() {
+            m_view->setTool(circleTool);
         });
 
         setWindowTitle("AxiomX");
